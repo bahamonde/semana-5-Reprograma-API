5d811239a2770363eff1a3272a1034179301ff30
bahamonde <bahamondealine@gmail.com>
2019-10-08 21:07:22 -0300

    atualizacao dos books

--- API/src/controllers/alunasController.js
+++ API/src/controllers/alunasController.js
@@ -9,6 +9,13 @@ exports.get = (req, res) => {
 exports.getById = (req, res) => {
     const id = req.params.id
     console.log(id)
-    res.status(200).send(alunas.find(aluna => aluna.id == id))
+    res.status(200).send(alunas.find(aluna => aluna.id == id))    
+}
+
+exports.getBooks = (req, res) => {
+    const id = req.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+    console.log(aluna)
+    res.status(200).send('Bateu')
     
 }
